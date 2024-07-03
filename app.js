600a08e9e82cfa04db52c9273d7e2196e4d016ce
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-07-03 12:01:46 +0530

    # origin chnged to all

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 // CORS setup
 const corsOptions = {
-  origin: 'http://localhost:3000', // replace with your client URL
+  origin: '*', // replace with your client URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
