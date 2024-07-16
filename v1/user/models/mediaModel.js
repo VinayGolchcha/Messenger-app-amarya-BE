3b8f63573002cbe691efe1a482cfca3be1ee7c7b
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-07-16 16:33:15 +0530

    # file type text omition

--- v1/user/models/mediaModel.js
+++ v1/user/models/mediaModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import timestamps from 'mongoose-timestamp';
 
 const MediaSchema = new mongoose.Schema({
-    file_type: { type: String, enum: ['text', 'image', 'audio', 'video', 'document'], required: true},
+    file_type: { type: String, enum: ['image', 'audio', 'video', 'document'], required: true},
     file_name: { type: String},
     file_data: { type: Buffer, required: true },
     uploaded_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
