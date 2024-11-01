11e6b52f391aee7e550a6bd3b9ed646fbc4e06bb
Sanjana_Jain022 <sanjana.jain@shephertz.com>
2024-11-01 13:53:27 +0530

    # User input to table from admin to messenger

--- v1/user/controllers/userController.js
+++ v1/user/controllers/userController.js
@@ -16,33 +16,230 @@ dotenv.config();
 
 export const userInput = async (req, res) => {
     try {
-        const userData = [{
-            username: "Sanjana",
-            email: "sanjana@yahoo.com",
-            password: '$2b$12$Bb2hulXzeapnkrFNfDl5SOL7Xd0lAXo9SrolmQ5.KHtsIk1EKOiqS',
-            is_registered: 1,
-        }, {
-            username: "Ashu",
-            email: "ashu@yahoo.com",
-            password: '$2b$12$Bb2hulXzeapnkrFNfDl5SOL7Xd0lAXo9SrolmQ5.KHtsIk1EKOiqS',
-            is_registered: 1
-        },
-        {
-            username: "Prabal",
-            email: "prabal@yahoo.com",
-            password: '$2b$12$Bb2hulXzeapnkrFNfDl5SOL7Xd0lAXo9SrolmQ5.KHtsIk1EKOiqS',
-            is_registered: 1,
-        }, {
-            username: "Aditya",
-            email: "aditya@yahoo.com",
-            password: '$2b$12$Bb2hulXzeapnkrFNfDl5SOL7Xd0lAXo9SrolmQ5.KHtsIk1EKOiqS',
-            is_registered: 1,
-        }, {
-            username: "Rashi",
-            email: "rashi@yahoo.com",
-            password: '$2b$12$Bb2hulXzeapnkrFNfDl5SOL7Xd0lAXo9SrolmQ5.KHtsIk1EKOiqS',
-            is_registered: 1,
-        }]
+        // const userData = [
+        //     {
+        //       "username": "tamanna_02",
+        //       "email": "tamanna.suhane@amaryaconsultancy.com",
+        //       "password": "$2b$12$.86ATInYxDAMwIkrCnmpnONLKjF/FmHcJjV5BDlR2NFXCHsr2VjAy",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "Ujjwal_01",
+        //       "email": "ujjwal.2000upadhyay@gmail.com",
+        //       "password": "undefined",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "sanjana_jain01",
+        //       "email": "sanjana.jain@amaryaconsultancy.com",
+        //       "password": "$2b$12$SmLEZpUF11nGaCDnIzvBAewNT7xMySDgGQcZvTSsptOUvIv5vJnmG",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "shubham_01",
+        //       "email": "shubham.sonii2019@gmail.com",
+        //       "password": "$2a$12$YZn6kjdZDVoJz.9rTHq4suGUF1qH85503zaq3y/EKv6C61HCtTFIe",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "anuj.prajapati01",
+        //       "email": "anuj.prajapati@amaryaconsultancy.com",
+        //       "password": "$2b$12$rURec.L4YJ5RWzfUhqynPeoxgBuRhGdndBqF3pRl.wsasQLvkk.72",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "shubham_soni01",
+        //       "email": "shubham.soni@amaryaconsultancy.com",
+        //       "password": "$2a$12$PUst9Oa86vgLnRA0dQac1ecuh3NkZ2lPMK55OWTGHk0lQQoX5ZJf6",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "ankit_koshta01",
+        //       "email": "ankit.koshta@amaryaconsultancy.com",
+        //       "password": "$2b$12$F7f23L3pgrN7JFPGG3G2peUmd.e3fAn3JslUAr2p.DEv/M365ckOu",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "shubham_kushwaha01",
+        //       "email": "shubham.kushwaha@amaryaconsultancy.com",
+        //       "password": "$2a$12$DyrOvzCvNt/oIc8MFRJwg.xZ15xxgGem0lhm8LBZO90u2tQRqkEya",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "prashant_pandey01",
+        //       "email": "prashant.pandey@amaryaconsultancy.com",
+        //       "password": "$2a$12$8blJ9Q9OVElwFlTKmmASDOtGgPVgmtHhY4N/EK2HnV04jaAqBanY6",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "kishan_chaurasia01",
+        //       "email": "kisanlal.chourasia@amaryaconsultancy.com",
+        //       "password": "$2b$12$1OImzc06l8w4gn8kiBE1XeoydOQR2Vad3GBFpDDLSql/BsVWvvxCa",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "deepanshu_kushwaha01",
+        //       "email": "deepanshu.kushwaha@amaryaconsultancy.com",
+        //       "password": "$2b$12$bR.Zpu60ZKWXvPcgX/ev1uQPnLHDqjg9wzbbYhZd51sJx4fwoJzqq",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "ankit_soni01",
+        //       "email": "ankit.soni@amaryaconsultancy.com",
+        //       "password": "$2b$12$hjGKgrNBTbOexTEnnnYseu0qxr1kO8QD.xNbwkxNRbJOxQc.TgEOu",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "divij_sahu01",
+        //       "email": "divij.sahu@amaryaconsultancy.com",
+        //       "password": "$2b$12$RnFUO.alofR7O24n1bD4g.IPpgV.NDUaDxMxfA0Qq.sGMc.Jr2JYa",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "pujita_rao01",
+        //       "email": "pujita.rao@amaryaconsultancy.com",
+        //       "password": "$2b$12$OvBVFY1PocHS9N5V4bJOueXz1yKX2RpLoVo/JDLNEyQ8XITqUiWOW",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "surya_pratap01",
+        //       "email": "suryapratap.dheemar@amaryaconsultancy.com",
+        //       "password": "$2b$12$fbQGaSvMDhKozQB6CNuS5.R.3BXtGlmq8PBrm63tr8KuAg1wKQJqq",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "eish_nigam01",
+        //       "email": "eish.nigam@amaryaconsultancy.com",
+        //       "password": "$2b$12$4eXStVkzBrJ7Y2qItG6fEezENskwbGe3HrjxxbeUrtY.QmWyJtH4e",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "prabal_namdev01",
+        //       "email": "prabal.namdev@amaryaconsultancy.com",
+        //       "password": "$2b$12$PkKv07AIaB6LF8fC2BK1ZesaWUJXhJPoEU4zewaotmnS9m0o6BEjm",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "iteesh_dubey01",
+        //       "email": "iteesh.dubey@amaryaconsultancy.com",
+        //       "password": "$2b$12$DrLN3oQFAZ/Yu45X4SnCk.fDpQ.qIMAlhF9R7uGxWb.G4Qrk1eXmm",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "anmol_chauhan01",
+        //       "email": "anmol.chauhan@amaryaconsultancy.com",
+        //       "password": "$2b$12$7Xkld8Iy4YcABliyUdVMYOjYob7tLg1y1qGYFcnhO.8xwMDi3RHQ6",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "shubham.k_01",
+        //       "email": "shubham.amarya7@gmail.com",
+        //       "password": "$2b$12$c/ILAAHsrVTszedC54X.Q.srM7VgNTvnaCMy.j6Zc0cgbfSqzFUyS",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "sanjana_01",
+        //       "email": "sanjanajain2223@gmail.com",
+        //       "password": "$2b$12$1ffakkC54/Lx/Uv5Ev4bR.k8pmjaWlRAXH5IdANllvSFezcMQrGhy",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "tamanna_suhane01",
+        //       "email": "suhanetamanna@gmail.com",
+        //       "password": "$2b$12$XiveyXldQ2pzz975mBuc0ufFonyHxjZw4sbS.yXtIYn.mTiUL7JXy",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "golchcha_vinay",
+        //       "email": "vinay.golchcha@gmail.com",
+        //       "password": "$2b$12$7t1DxF1LeshAjzIq7FW6q.F6KuCRMwk0EhEBqDslrWtG/iL0r.l0C",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "test_user",
+        //       "email": "test@gmail.com",
+        //       "password": "$2b$12$GeS70wS68VeRpsuN2.TsW.Wz/5oO/8P2p4QPbZYu4bYvfPR0JPSjK",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "rashi_agarwal01",
+        //       "email": "rashi.agarwal@amaryaconsultancy.com",
+        //       "password": "$2b$12$aPA6mJLFIrxBAchL7ZEoFeyZcTJ3UQ1dWMkgJOkeW/ysHwQnRdg5i",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "madhur_garg01",
+        //       "email": "madhur.garg@amaryaconsultancy.com",
+        //       "password": "$2b$12$XJTxZmVdgCsfmZzuonv8IeftTYY713DMJuZ2zvidAVedeRRnjfXce",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "vinay_golchha01",
+        //       "email": "vinay.golchha@amaryaconsultancy.com",
+        //       "password": "$2b$12$BJM3b0WNmF3ZUdh2n9LUvuKE6XeKuWzJHwnG.vTmC3WAFr2AY2Ww6",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "lucky_soni01",
+        //       "email": "lucky.soni@amaryaconsultancy.com",
+        //       "password": "$2b$12$5tVqC1ISuldIkTlFPEnHFel1SSZHObeB.9PXj3cb7sCIX7.IC/Hca",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "pradyum_jaiswal01",
+        //       "email": "pradyum.jaiswal@amaryaconsultancy.com",
+        //       "password": "$2b$12$Fm9rDa8kZ4bCcV1M6COsRuMbKDppyviMiZEJB6wjdZYsO.nvFH0ey",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "vishwabhushan_dubey01",
+        //       "email": "vishwabhushan.dubey@amaryaconsultancy.com",
+        //       "password": "$2b$12$MnFr8Zzt/Y4005XFsoZcmenJe60nVrJnYeVZSZt.ZrOPUh4KASdeS",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "shivam_vishwakarma01",
+        //       "email": "shivam.vishwakarma@amaryaconsultancy.com",
+        //       "password": "$2b$12$jsVJxRnGv.Iibnq4sF3X7uwQDPmxWHTyhnYHCS7L58dsC6iozHS4y",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "devesh_gupta01",
+        //       "email": "devesh.gupta@amaryaconsultancy.com",
+        //       "password": "$2b$12$xgLP5mOOY0yZjrFNvMSNNObM2d3TtdshlkVOaKeq55ZhgYAXzGICO",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "aahana_sarkhel01",
+        //       "email": "aahana.sarkhel@amaryaconsultancy.com",
+        //       "password": "$2b$12$q9ho52YvV/cJeywRDnU9n.jTK9lMQDACI3.RxdbWDuXr7HdA9GgvG",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "himanshu_bachwani01",
+        //       "email": "himanshu.bachwani@amaryaconsultancy.com",
+        //       "password": "$2b$12$2EmZBrMVDzYuAiAx/C60Q.ODRgb2FVVgwy8DL6.SikNCok5eAexse",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "ankit_admin",
+        //       "email": "ankitk2499@gmail.com",
+        //       "password": "$2a$12$CqZ99iwJ4qKiy8QvLzI4Ru.zZY7uxCNG2JJR1EeEzqg287P6D360W",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "manish_patel01",
+        //       "email": "manish.patel@amaryaconsultancy.com",
+        //       "password": "$2b$12$f7LFTWNo8gtxfR0HcylM7.GnnCHZI2BcJ7qzKyCLF/C0dAStKBMt.",
+        //       "is_registered": 1
+        //     },
+        //     {
+        //       "username": "prashant_admin",
+        //       "email": "prashant.pandey@amaryasonsultancy.com",
+        //       "password": "$2a$12$EABbRS4eGOTxll8b270xEOK2jhgqvOcBtP8afCvb0jK/1xi0/qula",
+        //       "is_registered": 1
+        //     }
+        //   ]
         for(let i = 0; i < userData.length; i++) {
             await create(userData[i])
         }
